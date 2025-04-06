652b7ea934410c5d288036fcc9549f43e97ad1b3
Val Koi <95649560+valkoivo@users.noreply.github.com>
2025-04-06 16:00:25 +0200

    Add files via upload

--- js/docs/controls/buttons/docs_idlink_for_header.js
+++ js/docs/controls/buttons/docs_idlink_for_header.js
@@ -8,7 +8,7 @@
  * Copyright (c) 2025 Koi
 */
 
-import { KoiIdLink } from "../../../../libs/web-components-lib/controls/buttons/control_idbutton.js";
+import { KoiButtonNativeLinkSocket, KoiIdLink } from "../../../../libs/web-components-lib/controls/buttons/control_idbutton.js";
 
 export class DocsIdLinkForHeader extends KoiIdLink {
 
@@ -25,6 +25,20 @@ export class DocsIdLinkForHeader extends KoiIdLink {
 		this.data.setAction(
 			DocsIdLinkForHeader.getToggleSidebarActionCode()
 		);
+	}
+
+	_getHamburgerTemplate(){
+		return '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">' +
+			'<path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>' +
+		'</svg>';
+	}
+
+	_constructSocket(){
+		return new KoiButtonNativeLinkSocket({
+			holder: this,
+			link_class: this.getAttribute('btn_class'),
+			link_text: this.innerHTML + this._getHamburgerTemplate()
+		});
 	}
 
 }
